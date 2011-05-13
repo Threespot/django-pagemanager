7312e613e8c1c9f5ecd353a276acefb074c0f8d2
Chuck Harmston <chuck@chuckharmston.com>
2011-05-13 13:02:40 -0400

    Adds static files, templates, and template tags from demo

---
+++ pagemanager/static/js/change.js
@@ -0,0 +1,190 @@
+(function($){
+
+    $(function(){
+
+        $('input#id_title').attr('placeholder', 'Title');
+
+        (function(){
+
+            return {
+
+                // init each piece of functionality
+                'init': function(){
+                    window.app = this;
+                    for(piece in this){
+                        if(piece !== 'init') this[piece].init();
+                    }
+                },
+
+                'orderInlines': {
+                    '$groups': $('.inline-group'),
+                    'makeSortLink': function(){
+                        return $('<a></a>', {
+                            'href': '#',
+                            'click': this.toggleSortable,
+                            'text': 'Sort'
+                        });
+                    },
+                    'enumerate': function(){
+                        var self = app.orderInlines;
+                        self.$groups.each(function(index, element){
+                            $(element).find('.inline-related').each(function(index, element){
+                                $(element).find('input[id$="order"]').val(index+1);
+                            });
+                        });
+                    },
+                    'startSortable': function($group){
+                        var self = app.orderInlines,
+                            makeSummary = function(element){
+                                var text = $(element).find('input[type="text"]:first').val()
+                                return $('<div></div>', {
+                                    'class': 'summary',
+                                    'html': $('<span></span>', {
+                                        'text': Boolean(text) ? text : 'Untitled'
+                                    })
+                                });
+                            };
+                        $group.addClass('sorting');
+                        //self.ckSetCache();
+                        $group.find('.inline-related').each(function(index, element){
+                            makeSummary(element).appendTo(element);
+                        });
+                        $group.sortable('enable');
+                    },
+                    'stopSortable': function($group){
+                        var self = app.orderInlines;
+                        // self.ckUpdateFromCache();
+                        $group.find('.summary').remove();
+                        $group.removeClass('sorting');
+                        $group.sortable('disable');
+                    },
+                    // 'ckSetCache': function(){
+                    //     $.each(CKEDITOR.instances, function(){
+                    //         this.updateElement();
+                    //         $(this.element).data('ck', this.getData());
+                    //     });
+                    // },
+                    // 'ckUpdateFromCache': function(){
+                    //     $.each(CKEDITOR.instances, function(){
+                    //         var val = $(this.element).data('ck');
+                    //         if(this.getData() != val){
+                    //             this.setData(val);
+                    //         }
+                    //     });
+                    // },
+                    'toggleSortable': function(evt){
+                        evt.preventDefault();
+                        var self = app.orderInlines,
+                            $group = $(this).closest('.inline-group');
+                        if(self.sorting){
+                            self.stopSortable($group);
+                            self.sorting = false;
+                        }else{
+                            self.startSortable($group);
+                            self.sorting = true;
+                        }
+                    },
+                    'init': function(){
+                        var self = this;
+                        this.enumerate();
+                        this.sorting = false;
+                        this.$groups.each(function(index, element){
+                            $element = $(element);
+                            console.log($element);
+                            app.orderInlines.makeSortLink().appendTo($element.find('h2'));
+                            $element.sortable({
+                                'items': '.inline-related',
+                                'handle': '.summary',
+                                'stop': self.enumerate
+                            });
+                            $element.find('span').disableSelection();
+                            $element.sortable('disable');
+                        });
+                    }
+                },
+
+                'slugToggle': {
+                    '$container': $('fieldset.basics div.slug'),
+                    'generate_form': function(){
+                        var form = $('<div></div>', {
+                            'class': 'form'
+                        });
+                        this.$input.appendTo(form);
+                        var list = $('<ul></ul>', {
+                            'class': 'object-tools'
+                        }).appendTo(form);
+                        var listItem = $('<li></li>').appendTo(list);
+                        var ok = $('<a></a>', {
+                            'href': '#',
+                            'text': 'OK',
+                            'click': function(evt){
+                                evt.preventDefault();
+                                app.slugToggle.toggle();
+                            }
+                        }).appendTo(listItem);
+                        return form;
+                    },
+                    'generate_edit_link': function(){
+                        var listItem = $('<li></li>');
+                        var edit = $('<a></a>', {
+                            'href': '#',
+                            'text': 'Edit',
+                            'click': function(evt){
+                                evt.preventDefault();
+                                app.slugToggle.toggle();
+                            }
+                        }).appendTo(listItem);
+                        return listItem
+                    },
+                    'open': function(){
+                        this.isOpen = true;
+                        this.$container.addClass('open')
+                    },
+                    'close': function(){
+                        this.isOpen = false;
+                        this.$container.removeClass('open')
+                    },
+                    'toggle': function(){
+                        if(this.isOpen){
+                            this.close();
+                        }else{
+                            this.open();
+                        }
+                    },
+                    'reset': function(){
+                        this.$input.val(this.$input[0].defaultValue);
+                    },
+                    'sanitize': function(str){
+                        return str.toLowerCase().replace(/\s/gi, '-').replace(/[^a-z0-9-]/gi, '');
+                    },
+                    'sanitizeValue': function(){
+                        if($(this).val() == '') $(this).val(this.defaultValue);
+                        $(this).val(app.slugToggle.sanitize($(this).val()));
+                    },
+                    'updateText': function(){
+                        var val = $(this).val();
+                        if(val == '') val = this.defaultValue;
+                        app.slugToggle.$text.text(app.slugToggle.sanitize(val));
+                    },
+                    'init': function(){
+                        this.$container.addClass('active');
+                        if(!this.$container.find('.add').length){
+                            this.$text = this.$container.find('.text');
+                            this.$input = this.$container.find('input[type="text"]');
+                            this.$form = this.generate_form().insertAfter(this.$text);
+                            this.$tools = this.$container.find('> div > .object-tools');
+                            this.$editLink = this.generate_edit_link().prependTo(this.$tools);
+                            this.$input.keyup(this.updateText);
+                            this.$input.blur(this.sanitizeValue);
+                            this.close();
+                        }
+                    }
+                }
+
+            };
+
+        })().init();
+
+    });
+
+})(django.jQuery);
---
+++ pagemanager/templatetags/staticpages_admin_tags.py
@@ -0,0 +1,105 @@
+from django import template
+from django.contrib.admin.sites import AdminSite, site
+from django.contrib.contenttypes.models import ContentType
+from django.utils.safestring import mark_safe
+from django.utils.text import capfirst
+
+from navigation.models import Menu
+from staticpages.app_settings import STATICPAGES_PAGE_MODEL
+
+register = template.Library()
+
+
+class ObjNode(template.Node):
+
+    def render(self, context):
+
+        layout_ct = ContentType.objects.get_for_id(context['content_type_id'])
+        layout_class = layout_ct.model_class()
+
+        context['obj'] = layout_class.objects.get(pk=int(context['object_id']))
+        context['page'] = context['obj'].page.all()[0]
+
+        return ''
+
+@register.tag
+def load_obj(parser, token):
+    """
+    In any admin change_view, adds the object being changed to the context
+    """
+    return ObjNode()
+
+
+class MenuNode(template.Node):
+    def render(self, context):
+        context['navigation_menus'] = Menu.objects.all()
+        context['navigation_menu_model'] = Menu
+        return ''
+
+@register.tag
+def load_menus(parser, token):
+    """
+    In any admin change_view, adds the object being changed to the context
+    """
+    return MenuNode()
+
+
+class PagesNode(template.Node):
+    def render(self, context):
+        context['staticpages_pages'] = STATICPAGES_PAGE_MODEL.objects.all()
+        context['staticpages_page_model'] = STATICPAGES_PAGE_MODEL
+        return ''
+
+@register.tag
+def load_pages(parser, token):
+    """
+    In any admin change_view, adds the object being changed to the context
+    """
+    return PagesNode()
+
+
+class AppListNode(template.Node):
+
+    def render(self, context):
+        app_dict = {}
+        request = context['request']
+        user = request.user
+        for model, model_admin in site._registry.items():
+            app_label = model._meta.app_label
+            has_module_perms = user.has_module_perms(app_label)
+            hidden_model = hasattr(model, 'hide_from_applist') and \
+                model.hide_from_applist()
+            if has_module_perms and not hidden_model:
+                perms = model_admin.get_model_perms(request)
+                if True in perms.values():
+                    model_dict = {
+                        'name': capfirst(model._meta.verbose_name_plural),
+                        'admin_url': mark_safe('%s/%s/' % (app_label, model.__name__.lower())),
+                        'perms': perms,
+                    }
+                    if app_label in app_dict:
+                        app_dict[app_label]['models'].append(model_dict)
+                    else:
+                        app_dict[app_label] = {
+                            'name': app_label.title(),
+                            'app_url': app_label + '/',
+                            'has_module_perms': has_module_perms,
+                            'models': [model_dict],
+                        }
+
+        # Sort the apps alphabetically, then sort models alphabetically within
+        # each app
+        app_list = app_dict.values()
+        app_list.sort(key=lambda x: x['name'])
+        for app in app_list:
+            app['models'].sort(key=lambda x: x['name'])
+
+        context['app_list'] = app_list
+        return ''
+
+@register.tag
+def staticpages_app_list(parser, token):
+    """
+
+    """
+    return AppListNode()
