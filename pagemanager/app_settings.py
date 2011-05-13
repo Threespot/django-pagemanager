f43b9d5829432dac9fdb3e0b7cdcabec229c964f
Chuck Harmston <chuck@chuckharmston.com>
2011-05-13 12:54:53 -0400

    Adds app settings from demo

---
+++ pagemanager/app_settings.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db.models import get_model
+
+from util import get_staticpage_model, get_staticpage_modeladmin
+
+STATICPAGES_DEFAULT_TEMPLATE = getattr(settings,
+    'STATICPAGES_DEFAULT_TEMPLATE',
+    'staticpages/base.html'
+)
+STATICPAGES_PAGE_MODEL = get_staticpage_model()
+STATICPAGES_PAGE_MODELADMIN = get_staticpage_modeladmin()
