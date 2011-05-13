1b12fe9a9808e71aea4472c96fb1f5bc602c1aa2
Chuck Harmston <chuck@chuckharmston.com>
2011-05-13 12:55:11 -0400

    Adds sites from demo

---
+++ pagemanager/sites.py
@@ -0,0 +1,54 @@
+from django.contrib.contenttypes.generic import GenericStackedInline
+from django.core.exceptions import ImproperlyConfigured
+
+from staticpages.exceptions import AlreadyRegistered, NotRegistered
+
+
+class StaticPageSite(object):
+
+    def __init__(self):
+        self._registry = []
+
+    def __iter__(self):
+        return self._registry.__iter__()
+
+    def register(self, page_layout):
+        """
+        Registers a given page layout. If it has already been registered, this
+        will raise AlreadyRegistered. If it is an abstract class, this will
+        raise ImproperlyConfigured.
+        """
+        if hasattr(page_layout, '__iter__'):
+            for item in page_layout:
+                self.register(item)
+        else:
+            if hasattr(page_layout, '_meta.abstract') and not \
+                page_layout._meta.abstract:
+                raise ImproperlyConfigured('The page layout %s is abstract, so it '
+                    'cannot be registered' % page_layout.__name__)
+            if page_layout in self._registry:
+                raise AlreadyRegistered('The page layout %s has already been '
+                    'registered' % page_layout.__name__)
+            self._registry.append(page_layout)
+
+    def unregister(self, page_layout):
+        """
+        Unregisters a given page layout. If it has not been registered, this
+        will raise NotRegistered.
+        """
+        if page_layout not in self._registry:
+            raise NotRegistered('The page layout %s cannot be unregistered as '
+                'it has not been registered' % page_layout.__name__)
+        self._registry.remove(page_layout)
+
+    def get_by_name(self, name):
+        """
+        Returns a PageLayout class from the registry based on a passed name.
+        """
+        for layout in self._registry:
+            if layout._staticpages_meta.name == name:
+                return layout
+        return None
+
+
+static_page_site = StaticPageSite()
