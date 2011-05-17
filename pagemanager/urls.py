beaa28d01f0c7610a9c22495399b3fb0481fb4af
Chuck Harmston <chuck@chuckharmston.com>
2011-05-17 12:33:46 -0400

    Refactors url import to function identically to staticfiles'

--- pagemanager/urls.py
+++ pagemanager/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls.defaults import *
 
+from pagemanager.views import PageView
+
 urlpatterns = patterns('pagemanager.views',
     url(r'^pages/drag/$', 'drag_post', name="drag_post"),
 )
+
+def pagemanager_urlpatterns():
+    return patterns('',
+        (r'^(?P<path>.+)/$', PageView.as_view()),
+    )
