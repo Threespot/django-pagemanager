8f57d6d4c1aba11b81a1739c983268a997f932d3
James Stevenson <james.m.stevenson@gmail.com>
2011-06-24 11:19:40 -0400

    Adds an option to specify admin via a python path-style import string

--- pagemanager/util.py
+++ pagemanager/util.py
@@ -26,11 +26,25 @@ def get_pagemanager_modeladmin():
     pagemanager.models.Page or a subclass thereof.
     """
     if hasattr(settings, 'PAGEMANAGER_PAGE_MODELADMIN'):
-        if issubclass(settings.PAGEMANAGER_PAGE_MODELADMIN, PageAdmin):
-            return settings.PAGEMANAGER_PAGE_MODELADMIN
+        if isinstance(settings.PAGEMANAGER_PAGE_MODELADMIN, str):
+            from django.utils.importlib import import_module
+            imp_path = settings.PAGEMANAGER_PAGE_MODELADMIN.split(".")
+            cls_name = imp_path.pop()
+            imp_path = ".".join(imp_path)
+            try:
+                module = import_module(imp_path)
+            except ImportError:
+                pass
+            else:
+                if hasattr(imp_path, cls_name):
+                    return getattr(imp_path, cls_name)
         else:
-            raise ImproperlyConfigured(
-                'PAGEMANAGER_PAGE_MODELADMIN must be a subclass of '
-                'pagemanager.admin.PageAdmin'
-            )
+            if issubclass(settings.PAGEMANAGER_PAGE_MODELADMIN, PageAdmin):
+                return settings.PAGEMANAGER_PAGE_MODELADMIN
+            else:
+                raise ImproperlyConfigured(
+                    'PAGEMANAGER_PAGE_MODELADMIN must be a subclass of '
+                    'pagemanager.admin.PageAdmin'
+                )
     return PageAdmin
+
