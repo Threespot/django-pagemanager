22f720c04ec49483ff51add8850261d3ab404a22
Chuck Harmston <chuck@chuckharmston.com>
2011-07-15 10:30:36 -0400

    Adds HomepageView to handle requests made to the site root

--- pagemanager/views.py
+++ pagemanager/views.py
@@ -1,13 +1,15 @@
-from django.views.generic import DetailView
+from django.core.urlresolvers import reverse
 from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import DetailView
 
 from pagemanager import app_settings
 
 
-model = app_settings.PAGEMANAGER_PAGE_MODEL
-
-
 class PageView(DetailView):
+    """
+    View that displays a given page in the site.
+    """
     context_object_name = 'page'
     model = app_settings.PAGEMANAGER_PAGE_MODEL
 
@@ -23,11 +25,9 @@ class PageView(DetailView):
         return None
 
     def get_object(self, queryset=None):
-
         count = 1
         queryset = self.model.objects.all()
         split = self.kwargs['path'].split('/')
-
         while count <= len(split):
             newslug = split[count * -1:self.zero_is_none(count * -1 + 1)]
             queryset = queryset.filter(slug=newslug[0])
@@ -35,3 +35,22 @@ class PageView(DetailView):
                 raise Http404
             elif len(queryset) == 1:
                 return queryset[0]
+
+
+class HomepageView(DetailView):
+    """
+    View that displays the single page denoted as being the homepage.
+    """
+    context_object_name = 'page'
+    model = app_settings.PAGEMANAGER_PAGE_MODEL
+
+    def get_template_names(self):
+        if self.template_name:
+            return self.template_name
+        return [app_settings.PAGEMANAGER_DEFAULT_TEMPLATE]
+
+    def get_object(self):
+        try:
+            return self.model.objects.get(is_homepage=True)
+        except:
+            raise Http404
