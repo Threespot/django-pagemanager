edf69abf92ea4630d8babad8eb582cfca6c19386
Chuck Harmston <chuck@chuckharmston.com>
2011-07-15 10:35:22 -0400

    Adds migration for PlaceholderPage

---
+++ pagemanager/migrations/0003_auto__add_placeholderpage.py
@@ -0,0 +1,58 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'PlaceholderPage'
+        db.create_table('pagemanager_placeholderpage', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        ))
+        db.send_create_signal('pagemanager', ['PlaceholderPage'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'PlaceholderPage'
+        db.delete_table('pagemanager_placeholderpage')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'pagemanager.page': {
+            'Meta': {'unique_together': "(('parent', 'slug'),)", 'object_name': 'Page'},
+            'copy_of': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['pagemanager.Page']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_homepage': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'layout_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '99999', 'null': 'True', 'blank': 'True'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['pagemanager.Page']"}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '32', 'db_index': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'draft'", 'max_length': '32'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'visibility': ('django.db.models.fields.CharField', [], {'default': "'public'", 'max_length': '32'})
+        },
+        'pagemanager.placeholderpage': {
+            'Meta': {'object_name': 'PlaceholderPage'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['pagemanager']
